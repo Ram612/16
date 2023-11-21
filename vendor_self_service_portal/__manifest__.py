6bcdc9c6f9d0cfed4e5c947234bb7c79489a8e31
Ram612 <ram612fullstack@gmail.com>
2023-11-21 12:34:12 +0530

    Vendor self service portal intial commit.

---
+++ vendor_self_service_portal/__manifest__.py
@@ -0,0 +1,13 @@
+# __manifest__.py
+
+{
+    'name': 'Vendor Self-Service Portal',
+    'version': '16.0.1',
+    'summary': 'Vendor portal for forecasts and order adjustments',
+    'author': 'Ramkumar',
+    'depends': ['base', 'sale'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
